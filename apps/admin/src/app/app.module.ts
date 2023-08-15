c27ed653b0d26ed2aa72a612f23d0094658be1d7
Vitalii Klim <vij.klimenko@gmail.com>
2023-08-15 19:55:14 +0300

    adding theme nebular for admin project

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  NbChatModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbMenuModule,
+  NbSidebarModule,
+  NbToastrModule,
+  NbWindowModule,
+} from '@nebular/theme';
+import {ThemeModule} from "./@theme/theme.module";
+import {CoreModule} from "./@core/core.module";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    NbDialogModule.forRoot(),
+    NbWindowModule.forRoot(),
+    NbToastrModule.forRoot(),
+    NbChatModule.forRoot({
+      messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
+    }),
+    CoreModule.forRoot(),
+    ThemeModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Route } from '@angular/router';
+import {
+    NbAuthComponent,
+    NbLoginComponent,
+    NbLogoutComponent,
+    NbRegisterComponent,
+    NbRequestPasswordComponent,
+    NbResetPasswordComponent,
+} from '@nebular/auth';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'pages',
+        loadChildren: () => import('./pages/pages.module')
+            .then(m => m.PagesModule),
+    },
+    {
+        path: 'auth',
+        component: NbAuthComponent,
+        children: [
+            {
+                path: '',
+                component: NbLoginComponent,
+            },
+            {
+                path: 'login',
+                component: NbLoginComponent,
+            },
+            {
+                path: 'register',
+                component: NbRegisterComponent,
+            },
+            {
+                path: 'logout',
+                component: NbLogoutComponent,
+            },
+            {
+                path: 'request-password',
+                component: NbRequestPasswordComponent,
+            },
+            {
+                path: 'reset-password',
+                component: NbResetPasswordComponent,
+            },
+        ],
+    },
+    { path: '', redirectTo: 'pages', pathMatch: 'full' },
+    { path: '**', redirectTo: 'pages' },
+];
